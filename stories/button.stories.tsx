91c5a357ada6bcfab704a6e49eef9dad6b046eac
Ramtin <Ramtin.Abadi@gmail.com>
2021-02-14 06:23:51 +0530

    Added onClick action in Button StoryBook

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Button from '../src/button';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
 
 type ButtonProps = React.ComponentProps<typeof Button>
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
@@ -11,6 +12,7 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 export default {
     title: 'Button',
     component: Button,
+    parameters: { actions: { argTypesRegex: '^on.*' } },
 } as Meta;
 
 
@@ -18,7 +20,7 @@ export const Primary = Template.bind({});
 Primary.args = {
     color: 'primary',
     text: "Save",
-    onClick: () => { },
+    onClick: action("onClick"),
 } as ButtonProps;
 
 
