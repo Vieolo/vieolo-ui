a69ab60608952b60e93339cd08935ecb250033be
Ramtin <Ramtin.Abadi@gmail.com>
2021-05-31 21:36:55 +0530

    Added temporary setup for the dist command

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from './lib/button';
+import Chip from './lib/chip';
+import DropDownMenu from './lib/dropdown_menu';
+import FileInput from './lib/file_input';
+import IconButton from './lib/icon_button';
+import Input from './lib/input';
+import InputSet from './lib/input_set';
+import Modal from './lib/modal';
+import Textarea from './lib/textarea';
+import TextareaSet from './lib/textarea_set';
+import TimePicker from './lib/time_picker';
+import Select from './lib/select';
+import Switch from './lib/switch';
+import SwitchSet from './lib/switch_set';
+
+export { 
+    Button,
+    Chip,
+    DropDownMenu,
+    FileInput,
+    IconButton,
+    Input,
+    InputSet,
+    Modal,
+    Textarea,
+    TextareaSet,
+    TimePicker,
+    Select,
+    Switch,
+    SwitchSet
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/lib/select.tsx
@@ -0,0 +1,128 @@
+// React
+import React, { Fragment } from 'react';
+
+
+type SelectItemType = {
+    title: string,
+    value: string,
+    category?: string
+}
+
+type SelectProps = {
+    title: string,
+    items: SelectItemType[],
+    selectedItem: string,
+    onSelect: (value: string) => void,
+    error: boolean
+}
+
+
+export default class Select extends React.Component<
+    SelectProps,
+    {
+        open: boolean
+    }
+> {
+
+    container: React.RefObject<unknown>;
+
+    constructor(props: SelectProps) {
+        super(props as any);
+        this.state = {
+            open: false
+        };
+        this.container = React.createRef();
+    }
+
+    handleButtonClick = () => {
+        this.setState(state => {
+            return {
+                open: !this.state.open
+            };
+        });
+    };
+
+    handleClickOutside = (event: any) => {
+        if (this.container.current && !(this.container.current as any).contains(event.target)) {
+            this.setState({
+                open: false,
+            });
+        }
+    };
+
+
+    componentDidMount() {
+        document.addEventListener("click", this.handleClickOutside);
+        document.querySelector('main').style.overflow = 'hidden';
+    }
+    componentWillUnmount() {
+        document.removeEventListener("click", this.handleClickOutside);
+        document.querySelector('main').style.overflow = 'auto';
+    }
+
+    getSelectedItem(value: string) : SelectItemType {
+        return this.props.items.filter(i => i.value == value)[0]
+    }
+
+    render(): React.ReactNode {
+
+        let thisSelectedItem = this.getSelectedItem(this.props.selectedItem);
+
+        return (
+            <div className="vieolo-select" ref={this.container as any}>
+                <div className={`select-button${this.props.error ? ' select-button-error' : ''}`} onClick={() => {this.setState({open: true})}}>
+                    <p className="button-title">{this.props.title}</p>
+                    <p className="button-value">{thisSelectedItem ? thisSelectedItem.title : null}</p>
+                </div>
+
+                {
+                    this.state.open &&
+                    <div className="select-dropdown">
+                        {
+                            this.props.items.map(item => {
+                                return <SelectItem 
+                                    item={item}
+                                    isSelected={this.props.selectedItem == item.value}
+                                    onSelect={(t: SelectItemType) => {
+                                        this.setState({
+                                            open: false,
+                                        })
+                                        this.props.onSelect(t.value);
+                                    }} 
+                                    />
+                            })
+                        }
+                    </div>
+                }
+            </div>
+        )
+    }
+
+}
+
+
+function SelectItem(props: {
+    item: SelectItemType, 
+    isSelected: boolean, 
+    onSelect: (item: SelectItemType) => void    
+}) {
+
+    let className= "select-item";
+
+    if (props.isSelected) className += " select-item-selected";
+    if (props.item.category) className += " select-item-category";
+
+    return <Fragment>
+        {
+            props.item.category &&
+            <p className="category-name">{props.item.category}</p>
+        }
+        <div 
+            className={className} 
+            onClick={() => {props.onSelect(props.item)}}
+            > 
+            {props.item.title}
+        </div>
+    </Fragment> 
+
+}
