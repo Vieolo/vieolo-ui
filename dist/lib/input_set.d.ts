453e8358bfd9dfb7d7283f3f8d938b39e5254c89
Ramtin <Ramtin.Abadi@gmail.com>
2021-05-31 22:44:38 +0530

    Adjusted the build of components

---
+++ dist/lib/input_set.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react" />
+export default function TextareaSet(props: {
+    label: string;
+    placeholder?: string;
+    tip?: string;
+    error: boolean;
+    value: string;
+    onChange: (value: string) => void;
+    disabled?: boolean;
+    size?: 'small' | 'medium' | 'large' | 'full';
+    type?: 'text' | 'number' | 'password';
+}): JSX.Element;
---
+++ src/export.ts
@@ -0,0 +1,32 @@
+import Button from './lib/button';
+import Chip from './lib/chip';
+import DropDownMenu from './lib/dropdown_menu';
+import FileInput from './lib/file_input';
+import IconButton from './lib/icon_button';
+import Input from './lib/input';
+import InputSet from './lib/input_set';
+import Modal from './lib/modal';
+import Textarea from './lib/textarea';
+import TextareaSet from './lib/textarea_set';
+import TimePicker from './lib/time_picker';
+import Select from './lib/select';
+import Switch from './lib/switch';
+import SwitchSet from './lib/switch_set';
+
+
+export { 
+    Button,
+    Chip,
+    DropDownMenu,
+    FileInput,
+    IconButton,
+    Input,
+    InputSet,
+    Modal,
+    Textarea,
+    TextareaSet,
+    TimePicker,
+    Select,
+    Switch,
+    SwitchSet
+}
