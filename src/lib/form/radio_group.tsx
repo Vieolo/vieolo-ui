0f9fdb6d752ebb67d6d22633184b4f23bd143a73
Ramtin Abadi <ramtin.abadi@gmail.com>
2021-10-11 21:26:10 +0530

    fixes #50

---
+++ src/lib/form/radio_group.tsx
@@ -0,0 +1,40 @@
+import TypographyParagraphMedium from "../typography/typography_paragraph_medium";
+
+export type RadioButtonType = {
+    id: string,
+    button: string | React.ReactNode
+}
+
+export default function RadioGroup(props: {
+    value: string,
+    options: RadioButtonType[],
+    onOptionChange: (o: string) => void,
+    direction: 'vertical' | 'horizontal'
+}) {
+
+    let className = `vieolo-radio-group__radio-contents vieolo-radio-group__radio-contents--${props.direction}`;            
+
+    return <div className="vieolo-radio-group">
+        <div className={className}>
+            {
+                props.options.map((o: RadioButtonType) => {
+                    return <div 
+                        key={o.id} 
+                        className={`vieolo-radio-group__radio-button ${props.value === o.id ? "vieolo-radio-group__radio-button--selected" : ""}`} 
+                        onClick={() => {
+                            props.onOptionChange(o.id);
+                        }}
+                    >
+                        {
+                            typeof o.button === 'string'
+                                ? <TypographyParagraphMedium text={o.button} />
+                                : <>
+                                    {o.button}
+                                </>
+                        }
+                    </div>
+                })
+            }
+        </div>
+    </div>
+}
