cb3ea610e2736e401cbfb1ac2d4be8aa3380cb5b
Ramtin Abadi <ramtin.abadi@gmail.com>
2021-11-16 17:55:43 +0530

    Implemented the Vieolo design language to button

--- src/lib/button/button.tsx
+++ src/lib/button/button.tsx
@@ -9,17 +9,29 @@ export default function Button(props: {
 	color: 'primary' | 'secondary' | 'tertiary' | 'error' | 'success' | 'alert' | 'accessory-blue' | 'accessory-orange' | 'accessory-green',
 	startIcon?: React.ReactNode,
 	endIcon?: React.ReactNode,
-	fullWidth?: boolean,
-	size?: 'large' | 'medium' | 'small',
+	/** 
+	 * selects the subclass of the `vieolo-button`
+	 * default: medium 
+	 */
+	height?: 'large' | 'medium' | 'small',
+	/** default: content */
+	width?: 'content' | 'full',
 	className?: string,
 	toLowerCase?: boolean,
 	fontSize?: number,
 	style?: React.CSSProperties,
 	type?: 'button' | 'submit' | 'reset',
-	variant?: 'text' | 'outlined' | 'contained'
+	/** default: high */
+	emphasis?: 'high' | 'medium' | 'low' | 'none',
+	/** default: `vieolo-button` css class */
+	borderRadius?: 'full' | 'normal' | 'half' | 'none'
 }) {
 	let s: {[key: string]: any} = {};
-	let c: string = `vieolo-button`;
+	let h = props.height || 'medium';
+	let w = props.width || 'content';
+	let c: string = `vieolo-button vieolo-button--${h} vieolo-button--${w}-width vieolo-button--border-radius-${props.borderRadius || 'default'}`;
+	let e = props.emphasis || 'high';
+	
 
 	if (props.toLowerCase) {
 		s['textTransform'] = 'initial';		
@@ -29,14 +41,19 @@ export default function Button(props: {
 		s['fontSize'] = `${props.fontSize}px`;
 	}
 
-	if (props.className) c += " " + props.className;
-
-	if (props.color) {
-		c += " " + props.color + "-background";
-		c += " ripple-" + props.color 
+	if (e === 'high') {
+		c = `${c} background-color--${props.color}-normal ripple ripple--${props.color}-light color--${props.color}-text border--px-0`;
+	} else if (e === 'medium') {
+		c = `${c} background-color--${props.color}-light ripple ripple--${props.color}-normal  border--px-0`;
+	} else if (e === 'low') {
+		c = `${c} background-color--white border--${props.color}-light hover--${props.color}-light color--${props.color}-normal  border--px-2 border--solid`;
+	}else {
+		c = `${c} background-color--white border--white hover-border--${props.color}-light color--${props.color}-normal border--px-2 border--solid ripple--${props.color}-light`;
 	}
 	
 	if (props.disabled) c += " disabled";
+
+	if (props.className) c += " " + props.className;
 
 	return <button className={c} onClick={props.onClick} style={s}>
 		{
--- src/view/button/button.tsx
+++ src/view/button/button.tsx
@@ -1,5 +1,5 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
 
 // Material UI
 import IconOne from '@mui/icons-material/Assignment';
@@ -9,12 +9,16 @@ import IconOne from '@mui/icons-material/Assignment';
 
 // Component
 import Button from '../../lib/button/button';
+import Select from '../../lib/form/select';
+
+// Types
+import { ColorOptionType } from '../../lib/private/types';
 
 type ButtonPropsType = React.ComponentProps<typeof Button>;
 
 export function buttonOptions(): { [key: string]: ButtonPropsType } {
 
-    let baseProps: ButtonPropsType = {        
+    let baseProps: ButtonPropsType = {
         onClick: () => alert("Clicked"),
         color: 'primary',
         text: "Button"
@@ -24,57 +28,119 @@ export function buttonOptions(): { [key: string]: ButtonPropsType } {
         "Base": {
             ...baseProps
         },
-        "Color -- Primary": {
-            ...baseProps,
-            color: 'primary'
-        },
-        "Color -- Secondary": {
-            ...baseProps,
-            color: 'secondary'
-        },
-        "Color -- Tertiary": {
-            ...baseProps,
-            color: 'tertiary'
-        },
-        "Color -- Alert": {
-            ...baseProps,
-            color: 'alert'
-        },
-        "Color -- Error": {
-            ...baseProps,
-            color: 'error'
-        },
-        "Color -- Success": {
-            ...baseProps,
-            color: 'success'
-        },
-        "Color -- Accessory Blue": {
-            ...baseProps,
-            color: 'accessory-blue'
-        },
-        "Color -- Accessory Green": {
-            ...baseProps,
-            color: 'accessory-green'
-        },
-        "Color -- Accessory Orange": {
-            ...baseProps,
-            color: 'accessory-orange'
-        },
-        
     }
 }
 
 
-export function ButtonCreator(props: {p: ButtonPropsType}) {
+export function ButtonCreator(props: { p: ButtonPropsType }) {
 
-    return <Button
-        onClick={props.p.onClick}
-        className={props.p.className}
-        color={props.p.color}
-        disabled={props.p.disabled}
-        size={props.p.size}
-        text={props.p.text}
-    />
+    let [color, setColor] = useState<ColorOptionType>('primary');
+    let [emphasis, setEmphasis] = useState<string>('high');
+    let [height, setHeight] = useState<string>('medium');
+    let [width, setWidth] = useState<string>('content');
+    let [borderRadius, setBorderRadius] = useState<string>('default');
+
+    return <div className="grid-two-column">
+
+        <div>
+            <Select
+                error={false}
+                items={(['primary', 'secondary', 'tertiary', 'error', 'success', 'alert', 'accessory-blue', 'accessory-green', 'accessory-orange'] as ColorOptionType[]).map(c => {
+                    return {
+                        title: c,
+                        value: c
+                    }
+                })}
+                onSelect={v => setColor(v[0] as any)}
+                selectedItems={[color]}
+                title={"Color"}
+            />
+
+            <br />
+            <br />
+
+            <Select
+                error={false}
+                items={(['high', 'medium', 'low', 'none']).map(c => {
+                    return {
+                        title: c,
+                        value: c
+                    }
+                })}
+                onSelect={v => setEmphasis(v[0] as any)}
+                selectedItems={[emphasis]}
+                title={"Emphasis"}
+            />
+
+            <br />
+            <br />
+
+            <Select
+                error={false}
+                items={(['large', 'medium', 'small']).map(c => {
+                    return {
+                        title: c,
+                        value: c
+                    }
+                })}
+                onSelect={v => setHeight(v[0] as any)}
+                selectedItems={[height]}
+                title={"Height"}
+            />
+
+            <br />
+            <br />
+
+            <Select
+                error={false}
+                items={(['content', 'full']).map(c => {
+                    return {
+                        title: c,
+                        value: c
+                    }
+                })}
+                onSelect={v => setWidth(v[0] as any)}
+                selectedItems={[width]}
+                title={"Width"}
+            />
+
+            <br />
+            <br />
+
+            <Select
+                error={false}
+                items={(['default', 'full', 'normal', 'half', 'none']).map(c => {
+                    return {
+                        title: c,
+                        value: c
+                    }
+                })}
+                onSelect={v => setBorderRadius(v[0] as any)}
+                selectedItems={[borderRadius]}
+                title={"Border Radius"}
+            />
+        </div>
+
+        <div>
+            <Button
+                onClick={props.p.onClick}
+                className={props.p.className}
+                color={color}
+                disabled={props.p.disabled}
+                text={props.p.text}
+                borderRadius={borderRadius as any}
+                emphasis={emphasis as any}
+                endIcon={props.p.endIcon}
+                fontSize={props.p.fontSize}
+                height={height as any}
+                startIcon={props.p.startIcon}
+                style={props.p.style}
+                toLowerCase={props.p.toLowerCase}
+                type={props.p.type}
+                width={width as any}
+            />
+        </div>
+    </div>
 
 }
 
