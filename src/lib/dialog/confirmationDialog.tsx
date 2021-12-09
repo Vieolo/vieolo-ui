4d9606978fab34aa1d3d376bed0913681372fabc
Ramtin Abadi <ramtin.abadi@gmail.com>
2021-12-09 22:40:46 +0530

    Added confirmation dialog

---
+++ src/lib/dialog/confirmationDialog.tsx
@@ -0,0 +1,84 @@
+// React
+import { Fragment } from "react"
+
+// Typography
+
+// Vieolo UI
+import { ColorOptionType, EmphasisType, BorderRadiusType } from "../private/types"
+import Modal from "./modal"
+import Button from '../button/button';
+import { TypographyParagraphLarge } from "../typography";
+
+
+export type ConfirmationDialogMainButton = {
+    text?: string,
+    color?: ColorOptionType,
+    emphasis?: EmphasisType,
+    borderRadius?: BorderRadiusType
+}
+
+export default function FormDialog(props: {
+    /** Function to be triggered when the user clicks cancel or closes the modal */
+    onReject?: () => void,
+    rejectButtonConfig?: ConfirmationDialogMainButton,
+    acceptButtonConfig?: ConfirmationDialogMainButton,
+    /** defaults to 10 */
+    padding?: number,
+    width: number | string,
+    onAccept: () => void,
+    removeRejectButton?: boolean,
+    message?: string | React.ReactNode,
+    /** If true, will not display the dialog as a modal */
+    inline?: boolean
+}) {
+
+    let dialog = <div className="vieolo-confirmation-dialog">
+
+        <div className="vieolo-form-dialog__message" style={{ width: props.width, padding: props.padding === undefined ? 10 : props.padding }}>
+            {
+                typeof props.message === 'string'
+                    ? <TypographyParagraphLarge text={props.message} />
+                    : <>
+                        {props.message}
+                    </>
+            }
+        </div>
+
+        <div className="vieolo-confirmation-dialog__footer">
+            <div className="vieolo-confirmation-dialog__footer__spacer--left"></div>
+            {
+                !props.removeRejectButton &&
+                <>
+                    <Button
+                        onClick={() => {
+                            if (props.onReject) props.onReject();
+                        }}
+                        color={(props.rejectButtonConfig && props.rejectButtonConfig.color) ? props.rejectButtonConfig.color : "error"}
+                        text={(props.rejectButtonConfig && props.rejectButtonConfig.text) ? props.rejectButtonConfig.text : "Cancel"}
+                        borderRadius={(props.rejectButtonConfig && props.rejectButtonConfig.borderRadius) ? props.rejectButtonConfig.borderRadius : undefined}
+                        emphasis={(props.rejectButtonConfig && props.rejectButtonConfig.emphasis) ? props.rejectButtonConfig.emphasis : "none"}
+                    />
+                    <div className="vieolo-form-dialog__footer__spacer--middle"></div>
+                </>
+            }
+
+            <Button
+                onClick={props.onAccept}
+                color={(props.acceptButtonConfig && props.acceptButtonConfig.color) ? props.acceptButtonConfig.color : "primary"}
+                text={(props.acceptButtonConfig && props.acceptButtonConfig.text) ? props.acceptButtonConfig.text : "Ok"}
+                borderRadius={(props.acceptButtonConfig && props.acceptButtonConfig.borderRadius) ? props.acceptButtonConfig.borderRadius : undefined}
+                emphasis={(props.acceptButtonConfig && props.acceptButtonConfig.emphasis) ? props.acceptButtonConfig.emphasis : undefined}
+            />
+        </div>
+    </div>
+
+    if (props.inline) return dialog;
+
+    return <Modal
+        onClose={() => {
+            if (props.onReject) props.onReject();
+        }}
+    >
+        {dialog}
+    </Modal>
+}
