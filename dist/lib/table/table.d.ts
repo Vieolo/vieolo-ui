50f176d5e3b4efb4e94bf605ed83d11b87263649
Ramtin Abadi <ramtin.abadi@gmail.com>
2021-12-31 00:34:32 +0530

    Version Bump to 0.14.0

--- dist/lib/table/table.d.ts
+++ dist/lib/table/table.d.ts
@@ -1,12 +1,29 @@
 import React from 'react';
 export declare type TableSortDirection = 'ascending' | 'descending';
+export declare type TableRow = {
+    id: string;
+    items: React.ReactNode[];
+    /**
+     * If a function is passed, the entire row will be clickable.
+     * It is recommended to avoid using clickable components inside the row when making the row clickable
+     */
+    onClick?: () => void;
+    /**
+     * If the table is not checkable, this property is ignored
+     */
+    checked?: boolean;
+    /**
+     * If the table is not checkable, this property is ignored
+     */
+    onCheckChange?: () => void;
+};
 export default function Table(props: {
     headers?: string[];
     /**
      * @deprecated The headers row is now optional, leaving which will result in absense of header row
      */
     removeHeaderRow?: boolean;
-    rows: React.ReactNode[][];
+    rows: TableRow[];
     columnGrid: string;
     disableSort?: boolean;
     sortBy?: string;
@@ -26,11 +43,6 @@ export default function Table(props: {
      */
     isDense?: boolean;
     /**
-     * If a functin is passed, the entire row will be clickable.
-     * It is recommended to avoid using clickable components inside the row when making the row clickable
-     */
-    onRowClick?: (index: number) => void;
-    /**
      * If the max height prop is provided, the pagination will appear below the max height,
      * adding to the total height
      */
@@ -44,4 +56,14 @@ export default function Table(props: {
         onPageChange: (newPage: number) => void;
         onPageItemCountChange?: (newCount: number) => void;
     };
+    /**
+     * If true, all rows will have a checkbox in the start, allowing the user to check a row
+     * A column of 30px will be added to the beginning of the column grid
+     */
+    isCheckable?: boolean;
+    /**
+     * The on change function of the checkbox in the header
+     * User can check or uncheck all of the items at once
+     */
+    onCheckAll?: (allAreChecked: boolean) => void;
 }): JSX.Element;
--- dist/lib/table/table.js
+++ dist/lib/table/table.js
@@ -1,4 +1,6 @@
 import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
+// React
+import { useState } from 'react';
 import IconButton from '../button/icon_button';
 // Typography
 import TypographyParagraphMedium from '../typography/typography_paragraph_medium';
@@ -6,7 +8,9 @@ import TypographyTitleSmall from '../typography/typography_title_small';
 // Material UI
 import LeftArrowIcon from '@mui/icons-material/ArrowLeft';
 import RightArrowIcon from '@mui/icons-material/ArrowRight';
+import Checkbox from '../form/checkbox';
 export default function Table(props) {
+    let [allChecked, setAllChecked] = useState(false);
     let style = {};
     let contentStyle = {};
     if (props.width) {
@@ -16,32 +20,51 @@ export default function Table(props) {
         contentStyle.maxHeight = props.maxHeight;
         contentStyle.overflowY = 'scroll';
     }
+    let columnGrid = props.columnGrid;
+    if (props.isCheckable)
+        columnGrid = `30px ${columnGrid}`;
     return _jsxs("div", Object.assign({ className: `vieolo-table ${props.removeHeaderRow ? 'vieolo-table--headless' : ''}`, style: style }, { children: [_jsxs("div", Object.assign({ className: "vieolo-table__content", style: contentStyle }, { children: [(props.removeHeaderRow !== true || Array.isArray(props.headers)) &&
-                        _jsx("div", Object.assign({ className: `vieolo-table__header-row ${props.stickyHeader ? 'position--sticky--top-0' : ''}`, style: { gridTemplateColumns: props.columnGrid } }, { children: (props.headers || []).map((h, i) => {
-                                return _jsxs("div", Object.assign({ className: "vieolo-table__header-row__cell", style: { cursor: (props.disableSort || !props.sortBy || !props.onSortChange || !props.sortDirection) ? 'default' : 'pointer' }, onClick: () => {
-                                        if (!props.disableSort && props.onSortChange && props.sortBy && props.sortDirection) {
-                                            props.onSortChange(h, props.sortBy === h ? props.sortDirection === 'ascending' ? 'descending' : 'ascending' : (props.defaultDirection || 'ascending'));
-                                        }
-                                    } }, { children: [_jsx(TypographyTitleSmall, { text: h }, void 0),
-                                        (props.sortBy === h && !props.disableSort) &&
-                                            _jsx(_Fragment, { children: props.sortDirection === 'ascending'
-                                                    ? _jsx("p", { children: "\u2193" }, void 0)
-                                                    : _jsx("p", { children: "\u2191" }, void 0) }, void 0)] }), `table_header_row_${i}`);
-                            }) }), void 0),
+                        _jsxs("div", Object.assign({ className: `vieolo-table__header-row ${props.stickyHeader ? 'position--sticky--top-0' : ''}`, style: { gridTemplateColumns: columnGrid } }, { children: [props.isCheckable &&
+                                    _jsx("div", Object.assign({ className: 'center-by-flex-row' }, { children: _jsx(Checkbox, { onChange: (v) => {
+                                                if (props.onCheckAll)
+                                                    props.onCheckAll(v);
+                                                setAllChecked(v);
+                                            }, value: allChecked }, void 0) }), void 0),
+                                (props.headers || []).map((h, i) => {
+                                    return _jsxs("div", Object.assign({ className: "vieolo-table__header-row__cell", style: { cursor: (props.disableSort || !props.sortBy || !props.onSortChange || !props.sortDirection) ? 'default' : 'pointer' }, onClick: () => {
+                                            if (!props.disableSort && props.onSortChange && props.sortBy && props.sortDirection) {
+                                                props.onSortChange(h, props.sortBy === h ? props.sortDirection === 'ascending' ? 'descending' : 'ascending' : (props.defaultDirection || 'ascending'));
+                                            }
+                                        } }, { children: [_jsx(TypographyTitleSmall, { text: h }, void 0),
+                                            (props.sortBy === h && !props.disableSort) &&
+                                                _jsx(_Fragment, { children: props.sortDirection === 'ascending'
+                                                        ? _jsx("p", { children: "\u2193" }, void 0)
+                                                        : _jsx("p", { children: "\u2191" }, void 0) }, void 0)] }), `table_header_row_${i}`);
+                                })] }), void 0),
                     _jsx("div", Object.assign({ className: (props.maxHeight && props.pagination) && "padding-bottom--40" }, { children: props.rows.map((row, i) => {
-                            let className = 'vieolo-table__content-row';
+                            let baseClass = 'vieolo-table__content-row';
+                            let className = `${baseClass}`;
                             if (props.isDense)
-                                className += ` ${className}--dense`;
-                            if (props.onRowClick)
+                                className += ` ${baseClass}--dense`;
+                            if (row.checked)
+                                className += ` ${baseClass}--checked`;
+                            if (row.onClick)
                                 className += ' clickable';
-                            return _jsx("div", Object.assign({ className: className, style: { gridTemplateColumns: props.columnGrid }, onClick: () => {
-                                    if (props.onRowClick)
-                                        props.onRowClick(i);
-                                } }, { children: row.map((r, z) => {
-                                    return _jsx("div", Object.assign({ className: "vieolo-table__content-row__cell" }, { children: typeof r === 'string'
-                                            ? _jsx(TypographyParagraphMedium, { text: r }, void 0)
-                                            : r }), `table_row_${i}_${z}_div`);
-                                }) }), `table_row_${i}`);
+                            return _jsxs("div", Object.assign({ className: className, style: { gridTemplateColumns: columnGrid }, onClick: () => {
+                                    if (row.onClick)
+                                        row.onClick();
+                                } }, { children: [props.isCheckable &&
+                                        _jsx("div", Object.assign({ className: 'center-by-flex-row' }, { children: _jsx(Checkbox, { onChange: (v) => {
+                                                    if (row.onCheckChange) {
+                                                        row.onCheckChange();
+                                                        setAllChecked(false);
+                                                    }
+                                                }, value: row.checked || false }, void 0) }), void 0),
+                                    row.items.map((r, z) => {
+                                        return _jsx("div", Object.assign({ className: "vieolo-table__content-row__cell" }, { children: typeof r === 'string'
+                                                ? _jsx(TypographyParagraphMedium, { text: r }, void 0)
+                                                : r }), `table_row_${row.id}_${z}_div`);
+                                    })] }), `table_row_${row.id}`);
                         }) }), void 0)] }), void 0),
             props.pagination &&
                 _jsx("div", Object.assign({ className: `vieolo-table__pagination-row ${props.maxHeight && 'position--sticky--bottom-0'}` }, { children: _jsxs("div", Object.assign({ className: "width--px-150 flex flex--space-around flex--align-items-center" }, { children: [_jsx(IconButton, { icon: _jsx(LeftArrowIcon, {}, void 0), onClick: () => props.pagination.onPageChange(props.pagination.pageNumber - 1), color: 'primary', size: 'small', disabled: props.pagination.pageNumber === 1 }, void 0),
