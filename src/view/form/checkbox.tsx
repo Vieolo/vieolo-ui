e60d7a2e74e55a950dbb476a798ddd660aa5f5ae
Ramtin Abadi <ramtin.abadi@gmail.com>
2021-12-30 21:20:57 +0530

    Removed the unused codes

--- src/view/form/checkbox.tsx
+++ src/view/form/checkbox.tsx
@@ -1,12 +1,6 @@
 // React
 import React, { useState } from 'react';
 
-// Material UI
-import IconOne from '@mui/icons-material/Assignment';
-import IconTwo from '@mui/icons-material/Backup';
-import IconThree from '@mui/icons-material/Cake';
-import IconFour from '@mui/icons-material/DataUsage';
-
 // Component
 import Checkbox from '../../lib/form/checkbox';
 
