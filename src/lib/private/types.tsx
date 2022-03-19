7e73b676f229efd069104f772e6f3812cf74b3cf
Ramtin <Ramtin.abadi@gmail.com>
2022-03-19 18:49:21 +0530

    Added elevation type

--- src/lib/private/types.tsx
+++ src/lib/private/types.tsx
@@ -3,3 +3,4 @@ export type EmphasisType = 'high' | 'medium' | 'low' | 'none';
 export type BorderRadiusType = 'full' | 'normal' | 'half' | 'none' | 'default';
 export type FontWeightType = 'light' | 'normal' | 'bold' | 'extra-bold';
 export type GridGapType = 'none' | 'half' | 'one' | 'two';
+export type ElevationType = '0' | '1' | '2';
