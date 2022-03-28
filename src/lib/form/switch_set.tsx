44ff306386ccf4c1a4b53fa2d04231b380cd58cc
Ramtin <Ramtin.abadi@gmail.com>
2022-03-28 13:50:36 +0530

    Added comment to the switch set

--- src/lib/form/switch_set.tsx
+++ src/lib/form/switch_set.tsx
@@ -20,6 +20,10 @@ export default function SwitchSet (props: {
     onChange: (v: boolean) => void,
     disabled?: boolean,
     switchID: string,
+    /**
+     * The height of the row
+     * To change the default height, override the "vieolo-swith-set--default-height" CSS class
+     */
     height?: RowHeightType | "default"
 }) {    
 
