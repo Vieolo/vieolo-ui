353437fe5e5e273e7fa2acaa04ffdc4494484c49
Ramtin <Ramtin.abadi@gmail.com>
2022-06-03 23:48:10 +0530

    Added color type to Spinner

--- src/lib/auxiliary/spinner.tsx
+++ src/lib/auxiliary/spinner.tsx
@@ -3,10 +3,11 @@ import { ColorOptionType } from "../private/types"
 
 export default function Spinner(props: {
     color?: ColorOptionType,
-    size?: 'extra-small' | 'small' | 'medium' | 'large'
+    size?: 'extra-small' | 'small' | 'medium' | 'large',
+    colorType?: 'normal' | 'text' | 'text-light' | 'light'
 }) {        
     
     return <svg className={`vieolo-spinner vieolo-spinner--${props.size || 'medium'}`} viewBox="0 0 50 50">
-        <circle className={`vieolo-spinner__path vieolo-spinner__path--${props.color || 'primary'}-normal`} cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+        <circle className={`vieolo-spinner__path vieolo-spinner__path--${props.color || 'primary'}-${props.colorType || 'normal'}`} cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
     </svg>
 }
