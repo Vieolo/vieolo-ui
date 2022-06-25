0fc5bafb633110b3c8690780963293bbee7899d5
Ramtin <Ramtin.abadi@gmail.com>
2022-06-26 01:50:39 +0530

    Improved the form section view

--- src/view/form/form_section.view.tsx
+++ src/view/form/form_section.view.tsx
@@ -1,5 +1,5 @@
 // React
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 // Component
 import FormSection from '../../FormSection';
@@ -11,29 +11,42 @@ import { ViewData } from '../main/main';
 type FormSectionPropsType = React.ComponentProps<typeof FormSection>;
 
 export function formSectionOptions(): ViewData {
-  return {
-    constants: {
-      title: "Contact Details",
-      secondaryValue: "",
-    } as Partial<FormSectionPropsType>,
-    variables: {
-      disabled: 'boolean',
-    }
+    return {
+        constants: {
+            title: "Contact Details",
+            secondaryValue: "",
+        } as Partial<FormSectionPropsType>,
+        variables: {
+            disabled: 'boolean',
+            showSecondaryValue: 'boolean'
+        }
 
-  }
+    }
 }
 
-export function FormSectionCreator(props: { p: FormSectionPropsType}) {
-  let [email, setEmail] = useState<string>('')
-  let [mobile, setMobile] = useState<string>('');
-  
-  useEffect(() => {
-      setEmail(email);
-      setMobile(mobile);
-  }, [email, mobile])
-  
-  return <FormSection title={props.p.title} disabled={props.p.disabled}> 
-    <InputSet label='Email' error={false} value={email} onChange={setEmail}/>
-    <InputSet label='Number' error={false} value={mobile} onChange={setMobile}/>
-  </FormSection>
+export function FormSectionCreator(props: { p: FormSectionPropsType }) {
+    let [email, setEmail] = useState<string>('')
+    let [mobile, setMobile] = useState<string>('');
+
+    return <div className='padding--one' style={{ backgroundColor: "#ddd" }}>
+        <FormSection 
+            title={props.p.title} 
+            disabled={props.p.disabled}
+            secondaryValue={(props.p as any).showSecondaryValue ? "Secondary Value" : undefined}
+        >
+            <InputSet
+                label='Email'
+                error={false}
+                value={email}
+                onChange={v => setEmail(v)}
+            />
+
+            <InputSet
+                label='Number'
+                error={false}
+                value={mobile}
+                onChange={v => setMobile(v)}
+            />
+        </FormSection>
+    </div>
 }
