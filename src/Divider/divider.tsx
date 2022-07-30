db1ae58de891ffebc85f523416a6e362a19e23c2
Ramtin <Ramtin.abadi@gmail.com>
2022-07-30 10:18:34 +0530

    Added thickness and padding to `Divider`

--- src/Divider/divider.tsx
+++ src/Divider/divider.tsx
@@ -6,18 +6,24 @@ export default function Divider(props: {
     length: WidthAndHeightSize,
     color?: ColorOptionType,
     colorType?: 'light' | 'normal' | 'text',
-    position?: 'start' | 'center' | 'end';
+    position?: 'start' | 'center' | 'end',
+    thickness?: "1" | "2" | 'half',
+    spaceAround?: "half" | 'one' | 'two'
 }) {
     // The base class
     let className = 'vieolo-divider';
     // The position class
     className += ` vieolo-divider--${props.direction} vieolo-divider--${props.position || 'start'}`
+
+    if (props.spaceAround) {
+        className += ` padding-${props.direction === 'horizontal' ? 'vertical' : 'horizontal'}--${props.spaceAround}`
+    }
     
     let dividerClass = "";
     // The length class
     dividerClass += ` ${props.direction === 'horizontal' ? 'width' : 'height'}--${props.length}`;
     // The thickness class
-    dividerClass += ` ${props.direction === 'horizontal' ? 'height' : 'widtg'}--half`;
+    dividerClass += ` vieolo-divider--thickness-${props.direction === 'horizontal' ? 'horizontal' : 'vertical'}--${props.thickness || 'half'}`;
     // The color class
     dividerClass += ` background-color--${props.color || 'primary'}-${props.colorType || 'light'}`;
     // The border radius class
