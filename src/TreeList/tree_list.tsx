75e4fef0d446cd1c5c8a636847d84d353beb32cd
Ramtin <Ramtin.abadi@gmail.com>
2022-12-23 20:15:13 +0530

    Added select callback to the `TreeList`

--- src/TreeList/tree_list.tsx
+++ src/TreeList/tree_list.tsx
@@ -14,27 +14,33 @@ import IconButton from "../IconButton";
 
 export type TreeListItem = {
     title: string,
+    id: string,
     icon?: React.ReactNode,
+    selected?: boolean,
     children?: TreeListItem[]
 }
 
 export default function TreeList(props: {
-    items: TreeListItem[]
+    items: TreeListItem[],
+    selectedID?: string,
+    onItemSelect: (id: string, path: string) => void,
 }) {
     return <div className="vieolo-tree-list">
         <Flex direction="column" rowGap="half">
             {
                 props.items.map(item => {
-                    return <SingleParent item={item} key={item.title} />
+                    return <SingleParent item={item} key={item.title} onItemSelected={props.onItemSelect} selectedId={props.selectedID} />
                 })
             }
         </Flex>
     </div>
 }
 
-function SingleParent(props: { item: TreeListItem }) {
+function SingleParent(props: { selectedId?: string, item: TreeListItem, onItemSelected: (id: string, path: string) => void }) {
     let [mode, setMode] = useState<"expanded" | "collapsed">("collapsed")
+    
     let hasChildren = props.item.children && props.item.children.length > 0
+    let isSelected = props.item.selected || (props.selectedId && props.item.id === props.selectedId)
 
     return <Card className="vieolo-tree-list-item" padding="none">
         <Flex alignItems="center">
@@ -46,9 +52,16 @@ function SingleParent(props: { item: TreeListItem }) {
                     size='extra-small'
                 />
             }
-            <Card emphasis="none-background" onClick={() => { }} padding='half' className="height--pc-100 width--pc-100 vieolo-tree-list-item__row">
+            <Card 
+                emphasis={isSelected ? "medium" : "none-background"} 
+                padding='half' 
+                className="height--pc-100 width--pc-100 vieolo-tree-list-item__row"
+                onClick={isSelected ? undefined : () => { 
+                    props.onItemSelected(props.item.id, props.item.title)
+                }}
+            >
                 <Flex className="height--pc-100" justifyContent="space-between" alignItems="center">
-                    <Typography text={props.item.title} type='title-small' />
+                    <Typography text={props.item.title} type='title-small' color={isSelected ? "primary" : undefined} colorType={isSelected ? 'text-light' : undefined} />
                     {
                         props.item.icon &&
                         props.item.icon
@@ -62,7 +75,7 @@ function SingleParent(props: { item: TreeListItem }) {
             <div className="vieolo-tree-list-item__children-container">
                 {
                     props.item.children!.map(c => {
-                        return <SingleParent item={c} key={c.title} />
+                        return <SingleParent onItemSelected={(id, p) => props.onItemSelected(id, `${props.item.title}/${p}`)} item={c} key={c.title} selectedId={props.selectedId} />
                     })
                 }
             </div>
--- src/TreeList/tree_list.view.tsx
+++ src/TreeList/tree_list.view.tsx
@@ -1,5 +1,5 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
 
 // Vieolo UI
 import TreeList from './tree_list';
@@ -28,32 +28,40 @@ export function treeListOptions(): ViewData {
 
 export function TreeListCreator(props: { p: TreeListPropsType }) {
 
+    let [selected, setSelected] = useState<string>("get_started/introduciton");
+
     return <div className='width--px-500 max-width--vw-90'>
         <TreeList
+            selectedID={selected}
+            onItemSelect={(id, p) => setSelected(id)}
             items={[
                 {
+                    id: "get_started",
                     title: "Get Started",
                     icon: <IconOne />,
                     children: [
-                        { title: "Introduction", icon: <IconThree /> },
-                        { title: "Team Members", children: [{ title: "Add" }, { title: "Edit" }, { title: "Delete" }] }
+                        { id: "get_started/introduciton", title: "Introduction", icon: <IconThree />, selected: true },
+                        { id: "get_started/team_members", title: "Team Members", children: [{ id: "get_started/add", title: "Add" }, { id: "get_started/edot", title: "Edit" }, { id: "get_started/delete", title: "Delete" }] }
                     ]
                 },
                 {
+                    id: "what_after",
                     title: "What you can do afterwards",
                     icon: <IconTwo />,
                     children: [
-                        { title: "Introduction", icon: <IconThree /> },
-                        { title: "Lots of things", children: [{ title: "Like this" }, { title: "or that" }, { title: "Or even this" }] },
+                        { id: "what_after/introduction", title: "Introduction", icon: <IconThree /> },
+                        { id: "what_after/lots", title: "Lots of things", children: [{ id: "what_after/introduction", title: "Like this" }, { id: "what_after/introduction", title: "or that" }, { id: "what_after/introduction", title: "Or even this" }] },
                         {
+                            id: "what_after/very nested",
                             title: "Very nested", children: [
                                 {
+                                    id: "what_after/introduction",
                                     title: "Parent 1", children: [
-                                        { title: "Child 1", children: [{ title: "GrandChild 1" }, { title: "GrandChild 2", children: [{ title: "Some other small child" }] }] }
+                                        { id: "what_after/introduction", title: "Child 1", children: [{ id: "what_after/introduction", title: "GrandChild 1" }, { id: "what_after/introduction", title: "GrandChild 2", children: [{ id: "what_after/introduction", title: "Some other small child" }] }] }
                                     ]
                                 },
-                                { title: "Edit" },
-                                { title: "Delete" }
+                                { id: "what_after/introduction", title: "Edit" },
+                                { id: "what_after/introduction", title: "Delete" }
                             ]
                         }
                     ]
