04e030b504342756e896d4cd19ebc2eafcf85d18
Ramtin <Ramtin.abadi@gmail.com>
2023-06-30 18:57:57 +0530

    fixes #298

--- src/RadioGroupRow/radio_group_row.tsx
+++ src/RadioGroupRow/radio_group_row.tsx
@@ -7,17 +7,26 @@ import RadioGroup from '../RadioGroup';
 import { RowHeightType } from '../types/types';
 
 //Private
-import SetRowTemplate from '../private/ComponentRowTemplate';
+import ComponentRowTemplate, {ComponentRowTemplateResponsive} from '../private/ComponentRowTemplate';
 
 type RadioGroupPropType = Omit<React.ComponentProps<typeof RadioGroup>, "direction"> 
 
 export default function RadioGroupRow(props: {
+    /** The label of the row, appearing on the left side */
     title: string | React.ReactNode,
+    /** The subtitle appears below the title */
     subtitle?: string | React.ReactNode,
     height?: RowHeightType | "default",    
+    /**
+     * Whether the title column should be positioned on the top of RadioGroup to accomodate for smaller screens.
+     * 
+     * You can explicitly determine the breakpoint from which the row is responsive. If a boolean is given, the breakpoint will be considered to the mobile layout
+     */
+    responsive?: ComponentRowTemplateResponsive
 } & RadioGroupPropType) {
-    return <SetRowTemplate
+    return <ComponentRowTemplate
         title={props.title}
+        responsive={props.responsive}
         subtitle={props.subtitle}
         handleKeyboardNav={false}
         className="vieolo-radio-group-row"
