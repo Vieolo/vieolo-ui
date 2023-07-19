066d1cd594f928ec7c19386a31df79d5f48a64bc
Ramtin <Ramtin.abadi@gmail.com>
2023-07-19 17:24:13 +0530

    Squashed merge from `improving_project`

--- version_bump.mjs
+++ version_bump.mjs
@@ -66,7 +66,7 @@ let changeLogLines = changelog.split('\n').slice(1);
 changeLogLines.unshift(...[
     "# Change Log",
     "",
-    `## ${fv} (${today()})`,
+    `## v${fv} (${today()})`,
     "- TODO: add the changes",
     "",
     "#### Deprecation",
