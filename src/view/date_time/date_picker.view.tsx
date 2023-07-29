b19cb8aa8511d481ef17fd2b2e5696bc479521f0
Ramtin <Ramtin.abadi@gmail.com>
2023-07-29 17:00:18 +0530

    fixes #286

--- src/view/date_time/date_picker.view.tsx
+++ src/view/date_time/date_picker.view.tsx
@@ -9,6 +9,10 @@ import DatePicker from '../../DatePicker';
 
 // Types
 import { ViewData } from '../main/main';
+import GridContainer from '../../GridContainer';
+import Grid from '../../Grid';
+import InputSet from '../../InputSet';
+import SelectSet from '../../SelectSet/select_set';
 
 type DatePickerPropsType = React.ComponentProps<typeof DatePicker>;
 
@@ -19,39 +23,71 @@ export function datePickerOptions(): ViewData {
 
         } as Partial<DatePickerPropsType>,
         variables: {
-            includeWeek: {
-                options: [false, true],
-                default: false
+            includeWeek: 'boolean',
+            showSelectedWeek: 'boolean',
+            showTitle: 'booleanTrueDefault',
+            disabled: 'boolean',
+            error: 'boolean',
+            width: {
+                options: ['small', 'medium', 'full'],
+                default: "medium"
             },
-            showSelectedWeek: {
-                options: [false, true],
-                default: false
-            },
-            showTitle: {
-                options: [false, true],
-                default: false
-            },
-            disabled: 'boolean'
+            dateFormat: {
+                options: ['yyyy-mm-dd', 'dd/mm/yyyy', 'mm/dd/yyyy'],
+                default: "dd/mm/yyyy"
+            }
         }
     }
 }
 
 
-export function DatePickerCreator(props: {p: DatePickerPropsType}) {
+export function DatePickerCreator(props: { p: DatePickerPropsType }) {
 
     let [date, setDate] = useState<VDate>(new VDate());
 
-    return <DatePicker
-        onDateSelect={v => setDate(v)}
-        includeWeek={props.p.includeWeek}
-        onWeekSelect={v => setDate(v)}
-        selectedDate={date}
-        title={(props.p as any).showTitle ? "Date" : undefined}
-        showSelectedWeek={props.p.showSelectedWeek}
-        disabled={props.p.disabled}
-        selectedWeek={{
-            startDate: date.getWeek().start,
-            weekNumber: date.getWeek().weekNumber
-        }}
-    />
+    return <GridContainer columnGap='one'>
+        <Grid xl={4} >
+            <DatePicker
+                onDateSelect={v => setDate(v)}
+                includeWeek={props.p.includeWeek}
+                error={props.p.error}
+                onWeekSelect={v => setDate(v)}
+                selectedDate={date}
+                title={(props.p as any).showTitle ? "Date" : undefined}
+                showSelectedWeek={props.p.showSelectedWeek}
+                disabled={props.p.disabled}
+                width={props.p.width}
+                dateFormat={props.p.dateFormat}
+                selectedWeek={{
+                    startDate: date.getWeek().start,
+                    weekNumber: date.getWeek().weekNumber
+                }}
+            />
+        </Grid>
+
+        <Grid xl={4}>
+
+            <InputSet
+                error={false}
+                label='For Comparison'
+                onChange={v => { }}
+                value=''
+                size='full'
+            />
+        </Grid>
+        
+        <Grid xl={4}>
+            <SelectSet 
+                error={false}
+                label='For Comparison'
+                onSelect={v => {}}
+                selectedItems={["one"]}
+                width='full'
+                items={[
+                    {title: "One", value: "one"}
+                ]}
+            />
+        </Grid>
+    </GridContainer>
+
 }
